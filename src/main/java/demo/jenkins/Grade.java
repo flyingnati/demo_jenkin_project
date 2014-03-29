81d88dde1e6fcfe6b17e3b72f97cbefc6ad73c6e
Nati <nati@flyingcomma.com>
2014-03-29 17:45:50 +0700

    Add test

--- src/main/java/demo/jenkins/Grade.java
+++ src/main/java/demo/jenkins/Grade.java
@@ -3,19 +3,19 @@ package demo.jenkins;
 public class Grade {
 
 	public String convert(int score) {
-		 if(score >= 85) {
+		if (score >= 85) {
 			return "A+";
 		}
-		 if(score >= 80) {
+		if (score >= 80) {
 			return "A";
 		}
-		 if(score >= 70) {
+		if (score >= 70) {
 			return "B";
 		}
-		 if(score >= 60) {
+		if (score >= 60) {
 			return "C";
 		}
-		 if(score >= 50) {
+		if (score >= 50) {
 			return "D";
 		}
 		return "F";
