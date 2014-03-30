6aa255d2081d9ca84d25d9e6e963c4495d5e97f5
Nati <nati@flyingcomma.com>
2014-03-30 09:38:32 +0700

    Add test

--- src/main/java/demo/jenkins/Grade.java
+++ src/main/java/demo/jenkins/Grade.java
@@ -27,4 +27,28 @@ return "F";
 //TODO
 //FIXME
 }
+
+public String cal(int score) {
+int ap = 85;
+int a = 80;
+int b = 70;
+int c = 60;
+int d = 50;
+if (score >= ap) {
+return "A+";
+}
+if (score >= a) {
+return "A";
+}
+if (score >= b) {
+return "B";
+}
+if (score >= c) {
+return "C";
+}
+if (score >= d) {
+return "D";
+}
+return "F";
+}
 }
